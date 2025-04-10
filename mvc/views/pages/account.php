2823329b093f2e245656fda23917063feaf2aa45
Jeemoua <tumouseyang@gmail.com>
2025-04-10 23:06:03 +0700

    change label

--- mvc/views/pages/account.php
+++ mvc/views/pages/account.php
@@ -24,7 +24,7 @@
 				<form>
 					<div class="py-2 px-4 cl bg2" style="width: 500px;margin: auto">
 						<p class="text-center" style="font-size: 130%">ປຽ່ນລະຫັດຜ່ານ</p>
-						<label>ລະຫັດເກົ່າ</label>
+						<label>Old Password</label>
 						<input id="passwordOld" type="text" class="form-control"  style="background: none;color: white">
 						<label class="mt-2">ລະຫັດໃໝ່</label>
 						<input id="passwordNew" type="text" class="form-control"  style="background: none;color: white">
